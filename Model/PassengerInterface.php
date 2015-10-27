69506b0b921351f802f5ee9d95f6e7f29595f78f
Королёв Олег <coroliov.o@fruitware.ru>
2015-10-27 12:44:53 +0200

    Added new titles for passenger (child and infant)

--- Model/PassengerInterface.php
+++ Model/PassengerInterface.php
@@ -4,6 +4,9 @@ namespace Fruitware\GabrielApi\Model;
 
 interface PassengerInterface extends CustomerInterface
 {
+    const TITLE_CHD  = 'CHD'; // for child
+    const TITLE_INF  = 'INF'; // for infant
+
     /**
      * One of the passengers can be the customer, if you already call setPassengers method
      *
